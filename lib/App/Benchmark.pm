5a660fb6c7f906ebc127b57ce790b203d3a0e263
Marcel Gruenauer <hanekomu@gmail.com>
2008-11-04 17:35:01 +0000

    export benchmark_diag() by default now
    git-svn-id: http://svn.coderepos.org/share/lang/perl/App-Benchmark/trunk@22727 d0d07461-0603-4401-acd4-de1884942a52

--- lib/App/Benchmark.pm
+++ lib/App/Benchmark.pm
@@ -13,11 +13,7 @@ our $VERSION = '0.02';
 use base 'Exporter';
 
 
-our %EXPORT_TAGS = (
-    util => [ qw/benchmark_diag/ ],
-);
-
-our @EXPORT_OK = @{ $EXPORT_TAGS{all} = [ map { @$_ } values %EXPORT_TAGS ] };
+our @EXPORT = ('benchmark_diag');
 
 
 sub benchmark_diag {
@@ -57,7 +53,7 @@ App::Benchmark - Output your benchmarks as test diagnostics
 
     # This is t/benchmark.t:
 
-    use App::Benchmark ':all';
+    use App::Benchmark;
 
     benchmark_diag(2_000_000, {
         sqrt => sub { sqrt(2) },
@@ -73,10 +69,7 @@ of perl.
 
 Ricardo Signes came up with the idea.
 
-=head1 EXPORTS
-
-Nothing is exported automatically. The function can be exported using its name
-or the C<:all> tag.
+=head1 FUNCTIONS
 
 =over 4
 
@@ -86,6 +79,8 @@ Takes a number of iterations and a benchmark definition hash, just like
 C<timethese()> from the L<Benchmark> module. Runs the benchmarks and reports
 them, each line prefixed by a hash sign so it doesn't mess up the TAP output.
 Also, a pseudotest is being generated to keep the testing framework happy.
+
+This function is exported automatically.
 
 =back
 
